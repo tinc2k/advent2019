24d52a510481fb06703ecdd7b8544d112ea181e0
Tin Crnkovic <tinc2k@gmail.com>
2019-12-01 11:39:37 +0100

    Day 1

---
+++ 01a.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let masses = fs.readFileSync('01input.txt', 'utf8').split('\n');
+
+fuel = 0;
+for (let mass of masses) {
+  fuel += getFuel(mass);
+}
+
+console.log(fuel);
+
+function getFuel(mass) {
+  return Math.floor(mass / 3) - 2;
+}
---
+++ 01b.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+let masses = fs.readFileSync('01input.txt', 'utf8').split('\n');
+
+let fuel = 0;
+for (let mass of masses) {
+  fuel += getTotalFuel(0, mass);
+}
+console.log(fuel);
+
+function getTotalFuel(total, leftover) {
+  let fuel = getFuel(leftover);
+  if (fuel <= 0) {
+    return total;
+  } else {
+    total += fuel;
+    leftover = fuel;
+    return getTotalFuel(total, leftover);
+  }
+}
+
+function getFuel(mass) {
+  let fuel = Math.floor(mass / 3) - 2;
+  return fuel;
+}
