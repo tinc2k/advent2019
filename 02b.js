5989bb4d36badbc95a6aafdce82046ee4f41d25c
Tin Crnkovic <tinc2k@gmail.com>
2019-12-03 13:33:39 +0100

    Minor pedantic changes

--- 02b.js
+++ 02b.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // https://adventofcode.com/2019/day/2#part2
 
 const fs = require('fs');
@@ -6,8 +8,8 @@ const WANTED = 19690720;
 
 let initial = fs.readFileSync('02input.txt', 'utf8').split(',').map(i => parseInt(i));
 
-for (let i = 0; i < 99; i++) {
-  for (let j = 0; j < 99; j++) {
+for (let i = 0; i <= 99; i++) {
+  for (let j = 0; j <= 99; j++) {
     let memory = initial.concat([]);
     // attempt this [i,j] combination
     memory[1] = i;
@@ -25,6 +27,7 @@ function run(memory) {
   do {
     // console.log(`instruction pointer is ${address}`);
     if (memory[address] === 1) {
+      // add
       let address1 = memory[address + 1];
       let address2 = memory[address + 2];
       let address3 = memory[address + 3];
@@ -32,6 +35,7 @@ function run(memory) {
       address += 4;
     }
     else if (memory[address] === 2) {
+      // multiply
       let address1 = memory[address + 1];
       let address2 = memory[address + 2];
       let address3 = memory[address + 3];
@@ -39,7 +43,9 @@ function run(memory) {
       address += 4;
     }
     else if (memory[address] === 99) {
+      // halt
       running = false;
+      address += 1;
     }
     else {
       console.error('Unknown opcode; something went wrong.', { address });
@@ -48,4 +54,3 @@ function run(memory) {
   // spit out memory after halting
   return memory;
 }
-
