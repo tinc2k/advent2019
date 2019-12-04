7ae1e2efbd4c5ec33b6776979c924ba45a01ec4d
Tin Crnkovic <tinc2k@gmail.com>
2019-12-04 14:18:20 +0100

    Minor cleanup.

--- 03b.js
+++ 03b.js
@@ -9,20 +9,11 @@ let paths = fs.readFileSync('03input.txt', 'utf8').split('\n');
 let path1 = paths[0].split(',');
 let path2 = paths[1].split(',');
 
-// console.log(path1);
-// console.log(path2);
-
-// render paths on map
 let map1 = renderPath(path1);
 let map2 = renderPath(path2);
-// console.log(map);
+
 let closest = findClosestIntersection(map1, map2);
 console.log(closest);
-
-// determine all overlaps on map
-
-// for each overlap, calculate manhattan distance from map center
-
 function renderPath(path) {
   let map = {};
   let x = 0;
@@ -31,7 +22,6 @@ function renderPath(path) {
   for (let instruction of path) {
     let direction = instruction[0];
     let distance = parseInt(instruction.substr(1));
-    // console.log(`going through instruction ${direction} for ${distance}.`);
     for (let i = 0; i < distance; i++) {
       if (direction === 'U') {
         y += 1;
@@ -51,9 +41,9 @@ function renderPath(path) {
       steps++;
       let coords = `${x},${y}`;
       if (map[coords]) {
-        console.log(`overlap with self at ${coords}.`);
-        //map[coords]++;
+        // console.log(`overlap with self at ${coords}.`);
       } else {
+        // each coordinate contains number of steps to get there
         map[coords] = steps;
       }
     }
@@ -65,26 +55,15 @@ function distanceFromCenter(x,y) {
   return Math.abs(x) + Math.abs(y);
 }
 
-
 function findClosestIntersection(map1, map2) {
   let closestDistance = null;
-  let closestX = null;
-  let closestY = null;
   for (let key in map1) {
     if (map2[key]) {
-      // we have overlap
-      // let coords = key.split(',').map(c => parseInt(c));
-      // let x = coords[0];
-      // let y = coords[1];
-      // console.log(`detected overlap at ${key} x: ${x}, y: ${y}`);
       let distance = distanceFromCenter(map1[key], map2[key]);
       if (closestDistance === null || distance < closestDistance) {
         closestDistance = distance;
-        // closestX = x;
-        // closestY = y;
       }
     }
-
   }
-  return { distance: closestDistance, x: closestX, y: closestY };
+  return closestDistance;
 }
