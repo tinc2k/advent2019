5fd435dbc1207fddb837cf42b93de8f21c93cc85
Tin Crnkovic <tinc2k@gmail.com>
2019-12-05 22:25:07 +0100

    Day 5 small cleanup

--- 05a.js
+++ 05a.js
@@ -5,57 +5,43 @@
 const fs = require('fs');
 
 let intcodes = fs.readFileSync('05input.txt', 'utf8').split(',').map(i => parseInt(i));
-// let intcodes = fs.readFileSync('05debug.txt', 'utf8').split(',').map(i => parseInt(i));
-
 
 run(intcodes, 1);
 
 function run(memory, input) {
   let running = true;
-
   let counter = 0;
   let output = null;
   do {
     let { opcode, mode1, mode2, mode3 } = decomposeOpcode(memory[counter]);
-    // console.log({ opcode, mode1, mode2, mode3 });
     if (opcode === 1) {
       // add
-      // console.log('add');
-      let address1 = memory[counter + 1];
-      let address2 = memory[counter + 2];
-      let address3 = memory[counter + 3];
-      // console.log({ address1, address2, address3 });
-      memory[address3] = (mode1 ? address1 : memory[address1]) + (mode2 ? address2 : memory[address2]);
+      let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
+      let param2 = mode2 ? memory[counter + 2] : memory[memory[counter + 2]];
+      let param3 = memory[counter + 3]
+      memory[param3] = param1 + param2;
       counter += 4;
-      // console.log(`result is ${memory[address3]}`);
     }
     else if (opcode === 2) {
       // multiply
-      // console.log('multiply');
-      let address1 = memory[counter + 1];
-      let address2 = memory[counter + 2];
-      let address3 = memory[counter + 3];
-      // console.log({ address1, address2, address3 });
-      memory[address3] = (mode1 ? address1 : memory[address1]) * (mode2 ? address2 : memory[address2]);
+      let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
+      let param2 = mode2 ? memory[counter + 2] : memory[memory[counter + 2]];
+      let param3 = memory[counter + 3]
+      memory[param3] = param1 * param2;
       counter += 4;
-      // console.log(`result is ${memory[address3]}`);
     }
-    /* Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50. */
     else if (opcode === 3) {
       // input
-      let address1 = parseInt(memory[counter + 1]);
+      let address1 = memory[counter + 1];
       memory[address1] = input;
       counter += 2;
     }
-    /* Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50. */
     else if (opcode === 4) {
       // output
-      let address1 = parseInt(memory[counter + 1]);
+      let address1 = memory[counter + 1];
       output = mode1 ? address1 : memory[address1];
       console.log(output);
-      if (mode1) {
-        console.log('we have output in mode1, sneaky');
-      }
+      if (mode1) console.log('we have output in mode1, sneaky', { opcode, mode1, address1 });
       counter += 2;
     }
     else if (opcode === 99) {
@@ -74,15 +60,11 @@ function run(memory, input) {
 
 function decomposeOpcode(opcode) {
   opcode = opcode.toString();
-  // if (opcode.toString) opcode = opcode.toString();
-  // console.log(`going through opcode ${opcode}.`);
   // prepend zeros if necessary
   if (opcode.length === 1) opcode = '0000' + opcode;
   else if (opcode.length === 2) opcode = '000' + opcode;
   else if (opcode.length === 3) opcode = '00' + opcode;
   else if (opcode.length === 4) opcode = '0' + opcode;
-
-  // console.log(`opcode now is ${opcode}.`);
 
   let command = parseInt(opcode.substr(3,5));
   let mode1 = opcode[2] === '1';
--- 05b.js
+++ 05b.js
@@ -5,8 +5,6 @@
 const fs = require('fs');
 
 let intcodes = fs.readFileSync('05input.txt', 'utf8').split(',').map(i => parseInt(i));
-// let intcodes = fs.readFileSync('05debug.txt', 'utf8').split(',').map(i => parseInt(i));
-
 
 run(intcodes, 5);
 
@@ -20,45 +18,34 @@ function run(memory, input) {
     // console.log({ opcode, mode1, mode2, mode3 });
     if (opcode === 1) {
       // add
-      // console.log('add');
       let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
       let param2 = mode2 ? memory[counter + 2] : memory[memory[counter + 2]];
       let param3 = memory[counter + 3]
-      // console.log({ param1, param2, param3 });
       memory[param3] = param1 + param2;
       counter += 4;
-      // console.log(`result is ${memory[address3]}`);
     }
     else if (opcode === 2) {
       // multiply
-      // console.log('multiply');
       let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
       let param2 = mode2 ? memory[counter + 2] : memory[memory[counter + 2]];
       let param3 = memory[counter + 3]
-      // console.log({ param1, param2, param3 });
       memory[param3] = param1 * param2;
       counter += 4;
-      // console.log(`result is ${memory[address3]}`);
     }
-    /* Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50. */
     else if (opcode === 3) {
       // input
       let address1 = memory[counter + 1];
       memory[address1] = input;
       counter += 2;
     }
-    /* Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50. */
     else if (opcode === 4) {
       // output
-      let address1 = memory[counter + 1];
-      output = mode1 ? address1 : memory[address1];
+      let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
+      output = param1;
       console.log(output);
-      if (mode1) {
-        console.log('we have output in mode1, sneaky');
-      }
+      if (mode1) console.log('we have output in mode1, sneaky', { opcode, mode1, address1 });
       counter += 2;
     }
-
     else if (opcode === 5) {
       // jump-if-true
       let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
@@ -79,7 +66,6 @@ function run(memory, input) {
         counter += 3;
       }
     }
-
     else if (opcode === 7) {
       // less-than
       let param1 = mode1 ? memory[counter + 1] : memory[memory[counter + 1]];
@@ -96,11 +82,10 @@ function run(memory, input) {
       memory[param3] = param1 === param2 ? 1 : 0;
       counter += 4;
     }
-
     else if (opcode === 99) {
       // halt
       running = false;
-      counter += 2;
+      counter += 1;
     }
     else {
       console.error('Unknown opcode; something went wrong.', { counter, opcode });
@@ -113,15 +98,11 @@ function run(memory, input) {
 
 function decomposeOpcode(opcode) {
   opcode = opcode.toString();
-  // if (opcode.toString) opcode = opcode.toString();
-  // console.log(`going through opcode ${opcode}.`);
   // prepend zeros if necessary
   if (opcode.length === 1) opcode = '0000' + opcode;
   else if (opcode.length === 2) opcode = '000' + opcode;
   else if (opcode.length === 3) opcode = '00' + opcode;
   else if (opcode.length === 4) opcode = '0' + opcode;
-
-  // console.log(`opcode now is ${opcode}.`);
 
   let command = parseInt(opcode.substr(3,5));
   let mode1 = opcode[2] === '1';
